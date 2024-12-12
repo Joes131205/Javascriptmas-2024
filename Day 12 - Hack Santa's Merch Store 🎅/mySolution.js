8507858616a5df10d06e4cc2899cf36b57ae9f49
Joes131205 <stevenjoe1312@gmail.com>
2024-12-12 11:34:37 +0700

    Day 12 - Hack Santa's Merch Store 🎅

---
+++ Day 12 - Hack Santa's Merch Store 🎅/mySolution.js
@@ -0,0 +1,15 @@
+// On the text area, paste the script
+
+// Task 1
+
+<button onclick="console.log('You have been hacked 🏴‍☠️')">Click me</button>;
+
+// Task 2
+<script>
+    document.querySelector(".prod-title").textContent = "Do not buy this";
+</script>;
+
+// Task 4
+<script>
+document.querySelector("#prod-buy").addEventListener("click", () => {console.log("Diverting payment to my account 💰");});
+</script>
